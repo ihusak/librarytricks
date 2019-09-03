961cd1aa0085644c3209fbad324a490c96f6c32d
Illia Husak <ilyagusak@gmail.com>
2019-09-03 18:58:13 +0300

    init dashboard branch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCrnxesCEc-xMbe5FLriyUYbTtotWX86NY",
+    authDomain: "librarytricksdev.firebaseapp.com",
+    databaseURL: "https://librarytricksdev.firebaseio.com",
+    projectId: "librarytricksdev",
+    storageBucket: "librarytricksdev.appspot.com",
+    messagingSenderId: "751358275465",
+    appId: "1:751358275465:web:fe2c92e5f1ac7aea"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCrnxesCEc-xMbe5FLriyUYbTtotWX86NY",
+    authDomain: "librarytricksdev.firebaseapp.com",
+    databaseURL: "https://librarytricksdev.firebaseio.com",
+    projectId: "librarytricksdev",
+    storageBucket: "librarytricksdev.appspot.com",
+    messagingSenderId: "751358275465",
+    appId: "1:751358275465:web:fe2c92e5f1ac7aea"
+  }
 };
 
 /*
