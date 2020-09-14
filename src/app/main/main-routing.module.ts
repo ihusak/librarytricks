c1584d0bc8acb8d680dd186be559fefc8adf23fe
Illia Husak <ilyagusak@gmail.com>
2020-09-15 01:08:48 +0300

    change profile logic

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -5,7 +5,6 @@ import { RouterModule, Routes} from '@angular/router';
 import { MainComponent } from './main.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { VideosComponent } from './components/videos/videos.component';
-import { ProgressComponent } from './components/progress/progress.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { RatingsComponent } from './components/ratings/ratings.component';
 
@@ -13,7 +12,6 @@ const routes: Routes = [
   {path: '', component: MainComponent, children: [
     {path: 'index', component: IndexComponent},
     {path: 'profile', loadChildren: () => import('./components/profile/profile.module').then(m => m.ProfileModule)},
-    {path: 'progress', component: ProgressComponent},
     {path: 'videos', component: VideosComponent},
     {path: 'tasks', loadChildren: () => import('./components/tasks/tasks.module').then(m => m.TasksModule)},
     {path: 'ratings', component: RatingsComponent}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -11,7 +11,6 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UserComponent } from './layouts/header/user.component';
 import { SidenavComponent } from './layouts/sidenav/sidenav.component';
 import { VideosComponent } from './components/videos/videos.component';
-import { ProgressComponent } from './components/progress/progress.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { RatingsComponent } from './components/ratings/ratings.component';
 import { MainService } from './main.service';
@@ -29,7 +28,6 @@ import { PipesModule } from '../shared/pipes/pipes.module';
     UserComponent,
     SidenavComponent,
     VideosComponent,
-    ProgressComponent,
     RatingsComponent,
     IndexComponent,
     AdminRequestPermissionPopupComponent
