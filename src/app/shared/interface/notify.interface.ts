f51693fcb42d1d343f30b89513cfc99ce7967b20
Illia Husak <ilyagusak@gmail.com>
2021-06-16 00:42:01 +0300

    LBJS-41. Notification template changes

--- src/app/shared/interface/notify.interface.ts
+++ src/app/shared/interface/notify.interface.ts
@@ -1,11 +1,15 @@
 export interface NotifyInterface {
   id?: string;
+  author: {
+    id: string;
+    name: string;
+  },
   users: [
     {
       id: string
     }
   ];
   title: string;
-  description: string;
+  type: string;
   userType: number;
 }
