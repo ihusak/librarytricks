24c3b6713b218dc79a1c71fa485110d150606d9c
Illia Husak <illia.husak@it.bekey.dk>
2021-07-07 19:05:16 +0300

    LBJS-28. Change create video layout

--- src/app/main/components/videos/videos.service.ts
+++ src/app/main/components/videos/videos.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
 import {AppService} from '../../../app.service';
 import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+export interface VideoInterface {
+  id: string;
+  description: string;
+  likes: string[];
+  createdBy: {
+    id: string;
+    name: string;
+  };
+  createdDate: string;
+  verified: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideosService extends AppService  {
-  public getAllVideos() {
+  public getAllVideos(): Observable<VideoInterface[]> {
     return this.http.get(`${this.apiUrl()}/videos/list`).pipe(map((videos: any) => {
       return videos;
     }));
