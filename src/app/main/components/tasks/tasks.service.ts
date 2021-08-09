0937dae4cce1ed10075fd713498734e59f5dea31
Illia Husak <illia.husak@it.bekey.dk>
2021-08-09 19:39:54 +0300

    LBJS-47. Implementing task select coach

--- src/app/main/components/tasks/tasks.service.ts
+++ src/app/main/components/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { TaskModel } from './task.model';
 import { map } from 'rxjs/operators';
 import { AppService } from 'src/app/app.service';
 import { CourseInterface } from 'src/app/shared/interface/course.interface';
+import {Observable} from 'rxjs';
 
 interface CourseCreateInterface {
   coachId: string;
@@ -16,28 +17,32 @@ interface CourseCreateInterface {
 @Injectable()
 export class TaskService extends AppService {
 
-    public getTaskById(id: string) {
+    public getTaskById(id: string): Observable<any> {
         return this.http.get(`${this.apiUrl()}/task/${id}`).pipe(map((task: any) => {
           return new TaskModel(task);
         }));
     }
-    public getAllTasks() {
+    public getAllTasks(): Observable<any> {
       return this.http.get(`${this.apiUrl()}/task/list`).pipe(map((tasks: any) => {
         return tasks.map(task => new TaskModel(task));
       }));
     }
 
-    public getTasksByCourse(courseId: string) {
+    public getTasksByCourse(courseId: string): Observable<any> {
       return this.http.get(`${this.apiUrl()}/task/course/${courseId}/list`).pipe(map((tasks: any) => {
         return tasks.map(task => new TaskModel(task));
       }));
     }
 
-    public getAllCourses() {
+    public getAllCourses(): Observable<any> {
       return this.http.get(`${this.apiUrl()}/courses`);
     }
 
-    public createCourse(values: CourseCreateInterface) {
+  public getCoachCourses(coachId: string): Observable<any> {
+    return this.http.get(`${this.apiUrl()}/courses/coach/${coachId}`);
+  }
+
+    public createCourse(values: CourseCreateInterface): Observable<any> {
       return this.http.post(`${this.apiUrl()}/courses/create-course`, {
         coachId: values.coachId,
         description: {
@@ -50,19 +55,19 @@ export class TaskService extends AppService {
       });
     }
 
-    public getStatusTasks(coachId: string, courseId: string, status: string) {
+    public getStatusTasks(coachId: string, courseId: string, status: string): Observable<any> {
       return this.http.post(`${this.apiUrl()}/task/status/coach/${coachId}/course/${courseId}`, {status});
     }
 
-    public createTask(task: TaskModel) {
+    public createTask(task: TaskModel): Observable<any> {
       return this.http.post(`${this.apiUrl()}/task/create`, task);
     }
 
-    public updateTask(taskId: string, task: TaskModel) {
+    public updateTask(taskId: string, task: TaskModel): Observable<any> {
       return this.http.put(`${this.apiUrl()}/task/update/${taskId}`, task);
     }
 
-    public deleteTask(taskId: string) {
+    public deleteTask(taskId: string): Observable<any> {
       return this.http.delete(`${this.apiUrl()}/task/delete/${taskId}`);
     }
 }
