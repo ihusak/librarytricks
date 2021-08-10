c95ff353b03c9150eaac20a54070697d1abcb45b
Illia Husak <ilyagusak@gmail.com>
2021-08-11 00:39:45 +0300

    LBJS-47. Courses improvements. Fix prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ import {version} from '../../package.json';
 export const environment = {
   production: true,
   api_url: 'https://lb.afreestylers.com/api',
-  version: version
+  version: version,
+  appName: 'Librarytricks'
 };
