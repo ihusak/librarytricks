531c0e80ece28c946fcfb8102bc2d61f0b931b78
Illia Husak <illia.husak@it.bekey.dk>
2021-08-13 21:32:50 +0300

    LBJS-48. Angular universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   LoaderComponent
   ],
   imports: [
-  BrowserModule,
+  BrowserModule.withServerTransition({ appId: 'serverApp' }),
   MatProgressSpinnerModule,
   AppRoutingModule,
   BrowserAnimationsModule,
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import 'hammerjs';
-
+// import 'hammerjs';
+import '@egjs/hammerjs';
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+document.addEventListener('DOMContentLoaded', () => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+});
