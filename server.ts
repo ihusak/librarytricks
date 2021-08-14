4e99062cbca8bc4676e47577d786425acc5df78c
Illia Husak <ilyagusak@gmail.com>
2021-08-14 16:30:53 +0300

    LBJS-48. Metatags pospone

--- server.ts
+++ server.ts
@@ -19,19 +19,18 @@ import 'zone.js/dist/zone-node';
 
 import * as express from 'express';
 import {join} from 'path';
-import {renderModuleFactory} from '@angular/platform-server';
 import {readFileSync} from 'fs';
+const path = require('path');
 
 // Express server
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
-
+const DIST_FOLDER = path.resolve(path.dirname(''));
 const indexHtml =
-    readFileSync(`dist/browser/index.html`, 'utf-8').toString();
+ readFileSync(path.join(DIST_FOLDER, 'browser/index.html')).toString();;
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap, renderModuleFactory} = require('./dist/server/main');
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 console.log('!!!!__dirname', indexHtml);
 app.engine('html', ngExpressEngine({
@@ -42,16 +41,17 @@ app.engine('html', ngExpressEngine({
 }));
 
 app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
+app.set('views', join(DIST_FOLDER, 'browser'));
 
 // Example Express Rest API endpoints
 // app.get('/api/**', (req, res) => { });
 // Serve static files from /browser
-app.get('*.*', express.static(DIST_FOLDER, {
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
   maxAge: '1y'
 }));
 
 app.route('*').get((req, res) => {
+  console.log('req.url', req.url);
   renderModuleFactory(AppServerModuleNgFactory, {
     document: indexHtml,
     url: req.url
