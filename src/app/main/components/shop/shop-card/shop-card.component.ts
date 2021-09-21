191b8cb9bd6c67fcbe60085a57ca56c160d7a603
Illia Husak <ilyagusak@gmail.com>
2021-09-22 01:16:36 +0300

    LBJS-49. Change basket logic

--- src/app/main/components/shop/shop-card/shop-card.component.ts
+++ src/app/main/components/shop/shop-card/shop-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {ProductInterface, ShopService} from '../shop.service';
 import {TranslateService} from '@ngx-translate/core';
 import {ActivatedRoute} from '@angular/router';
 
+interface ProductImages {
+  image: string;
+  thumbImage: string;
+  alt: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-shop-card',
   templateUrl: './shop-card.component.html',
-  styleUrls: ['./shop-card.component.scss']
+  styleUrls: ['./shop-card.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ShopCardComponent implements OnInit {
   public product: ProductInterface;
+  public productImages: ProductImages[] = [];
   constructor(
     private shopService: ShopService,
     private translateService: TranslateService,
@@ -20,7 +29,21 @@ export class ShopCardComponent implements OnInit {
     const ID = this.route.snapshot.paramMap.get('id');
     this.shopService.getProductById(ID).subscribe((product: ProductInterface) => {
       this.product = product;
+      this.productImages = this.product.images.map((url: string, i) => {
+        return {
+          image: `api/${url}`,
+          thumbImage: `api/${url}`,
+          alt: this.removeTags(product.description),
+          title: this.removeTags(product.title)
+        }
+      })
     });
   }
-
+  private removeTags(str) {
+    if ((str===null) || (str===''))
+        return false;
+    else
+        str = str.toString();
+    return str.replace( /(<([^>]+)>)/ig, '');
+}
 }
--- src/app/main/components/shop/shop.component.ts
+++ src/app/main/components/shop/shop.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject } from 'rxjs';
+import { ProductModel } from './product.model';
+import { ShopService } from './shop.service';
 
 @Component({
   selector: 'app-shop',
@@ -6,11 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-
+  public basket: ProductModel[] = [];
   constructor(
+    private shopService: ShopService,
+    private snackBar: MatSnackBar,
+    private translateService: TranslateService
   ) { }
 
   ngOnInit() {
+    this.shopService.busketObj.subscribe((basket: ProductModel[]) => {
+      this.basket = basket;
+      this.shopService.basketData = basket;
+    })
   }
-
+  public priceSum(products: ProductModel[], type: string): number {
+    return products.reduce((sum, current) => {
+      return sum + current[type];
+    }, 0)
+  }
+  public removeProduct(id: string) {
+    const selectedProducts = localStorage.getItem('addedProducts').split(',');
+    let result;
+    this.basket = this.basket.filter((product: ProductModel) => product.id !== id);
+    result = selectedProducts.filter(sid => sid !== id);
+    this.shopService.busketObj.next(this.basket);
+    localStorage.setItem('addedProducts', result);
+  }
 }
