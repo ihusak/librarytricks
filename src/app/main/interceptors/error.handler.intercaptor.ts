ae5677781c735d1dd412f5d089d8ace11910c08f
ihusak <illia.husak@it.bekey.dk>
2021-09-24 18:02:45 +0300

    LBJS-49. Shop. Fix error page intercaptor

--- src/app/main/interceptors/error.handler.intercaptor.ts
+++ src/app/main/interceptors/error.handler.intercaptor.ts
@@ -1,18 +1,21 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse} from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 @Injectable()
-export class ErrorHandlerIntercaptor implements HttpInterceptor {
+export class ErrorHandlerInterceptor implements HttpInterceptor {
 
   constructor(
     private router: Router
-    ){}
+    ) {}
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       return next.handle(req).pipe(
-        tap((event => {
+        tap(((event: HttpResponse<any>) => {
+          if (this.router.url.indexOf('error') > 0 && event.ok) {
+            this.router.navigate(['main/dashboard']);
+          }
           return event;
         }),
         (err) => {
@@ -22,5 +25,5 @@ export class ErrorHandlerIntercaptor implements HttpInterceptor {
             }
           }
         })
-    )};
+    )}
 }
